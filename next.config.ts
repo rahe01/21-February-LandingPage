3c98e6f55d3ece15e20d85da3e4088e912935bd5
rahe01 <rahatrahe01@gmail.com>
2026-03-07 00:17:27 +0600

    finised

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // keep static export
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // ✅ disables Image Optimization for static export
+  },
 };
 
 export default nextConfig;
